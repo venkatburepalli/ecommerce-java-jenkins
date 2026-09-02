2182b292ec143b0f5cc278c1a13c1f01c64efb9e
venkatburepalli <venkat24burepalli@gmail.com>
2026-09-02 17:57:31 +0530

    Modify Product class to include rating and mutable fields
    Updated Product class to allow mutable fields and added rating attribute.

--- src/main/java/com/practice/ecommerce/model/Product.java
+++ src/main/java/com/practice/ecommerce/model/Product.java
@@ -2,24 +2,23 @@ package com.practice.ecommerce.model;
 
 public class Product {
 
-    private final int id;
-    private final String name;
-    private final double price;
-    private final String imageUrl;
-    private final String description;
+    private int id;
+    private String name;
+    private double price;
+    private String imageUrl;
+    private String description;
+    private double rating;
 
-    public Product(
-            int id,
-            String name,
-            double price,
-            String imageUrl,
-            String description) {
+    public Product(int id, String name, double price,
+                   String imageUrl, String description,
+                   double rating) {
 
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
         this.description = description;
+        this.rating = rating;
     }
 
     public int getId() {
@@ -40,5 +39,9 @@ public class Product {
 
     public String getDescription() {
         return description;
+    }
+
+    public double getRating() {
+        return rating;
     }
 }
