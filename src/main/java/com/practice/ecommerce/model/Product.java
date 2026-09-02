0e8d6783e99aa00ba1c3a92bb1bd724c0ba4ef81
venkatburepalli <venkat24burepalli@gmail.com>
2026-09-02 17:46:15 +0530

    Add imageUrl and description fields to Product

--- src/main/java/com/practice/ecommerce/model/Product.java
+++ src/main/java/com/practice/ecommerce/model/Product.java
@@ -5,11 +5,21 @@ public class Product {
     private final int id;
     private final String name;
     private final double price;
+    private final String imageUrl;
+    private final String description;
 
-    public Product(int id, String name, double price) {
+    public Product(
+            int id,
+            String name,
+            double price,
+            String imageUrl,
+            String description) {
+
         this.id = id;
         this.name = name;
         this.price = price;
+        this.imageUrl = imageUrl;
+        this.description = description;
     }
 
     public int getId() {
@@ -22,5 +32,13 @@ public class Product {
 
     public double getPrice() {
         return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
